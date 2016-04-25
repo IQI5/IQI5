df424200de8f3ab7775e82ea1b443d551995f721
SunshineLXH <olixiaohui@163.com>
2016-04-25 23:54:27 +0800

    Update keyupGetArray.js

--- js/keyupGetArray.js
+++ js/keyupGetArray.js
@@ -6,41 +6,85 @@ var kDown, kUp, tOfDuration;
 var json = {}, arr=[];
 
 //按键按下时记录时间
-window.document.onkeydown = function(ev) {
-    ev = ev ? ev || window.event;
+document.onkeydown = function(ev) {
+    var ev = ev || window.event;
     kDown = ev.timeStamp;
 };
 
 //按键抬起时，查看键值，及按下的持续时间
-window.document.onkeyup = function(ev) {
-    ev = ev ? ev : window.event;
+document.onkeyup = function(ev) {
+    var ev = ev || window.event;
     kUp = ev.timeStamp;
     tOfDuration = kUp - kDown;
 
     switch(ev.keyCode) {
-        case 49:
-            addObj(1);
+        //按键QWERTY
+        case 81: //按键Q
+            addObj('Q');
             break;
-        case 50:
-            addObj(2);
+        case 87: //按键W
+            addObj('W');
             break;
-        case 51:
-            addObj(3);
+        case 69: //按键E
+            addObj('E');
             break;
-        case 52:
-            addObj(4);
+        case 82: //按键R
+            addObj('R');
             break;
-        case 53:
-            addObj(5);
+        case 84: //按键T
+            addObj('T');
             break;
-        case 54:
-            addObj(6);
+        case 89: //按键Y
+            addObj('Y');
             break;
-        case 55:
-            addObj(7);
+        case 85: //按键U
+            addObj('U');
             break;
-        case 48:
-            addObj(0);
+
+        //按键ASDFOHJ
+        case 65: //按键A
+            addObj('A');
+            break;
+        case 83: //按键S
+            addObj('S');
+            break;
+        case 68:  //按键D
+            addObj('D');
+            break;
+        case 70: //按键F
+            addObj('F');
+            break;
+        case 71: //按键G
+            addObj('G');
+            break;
+        case 72: //按键H
+            addObj('H');
+            break;
+        case 74: //按键J
+            addObj('J');
+            break;
+
+        //按键ZXCVBNM
+        case 90: //按键Z
+            addObj('Z');
+            break;
+        case 88: //按键X
+            addObj('X');
+            break;
+        case 67: //按键C
+            addObj('C');
+            break;
+        case 86: //按键V
+            addObj('V');
+            break;
+        case 66: //按键B
+            addObj('B');
+            break;
+        case 78: //按键N
+            addObj('N');
+            break;
+        case 77: //按键M
+            addObj('M');
             break;
     }
     console.log(arr);
